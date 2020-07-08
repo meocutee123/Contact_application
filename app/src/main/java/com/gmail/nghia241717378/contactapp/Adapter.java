812296aa083e3ffa93cda65702659593ac42ed25
meocutee123 <49399040+meocutee123@users.noreply.github.com>
2020-07-08 18:52:55 +0700

    sadsadas

--- app/src/main/java/com/gmail/nghia241717378/contactapp/Adapter.java
+++ app/src/main/java/com/gmail/nghia241717378/contactapp/Adapter.java
@@ -1,26 +1,29 @@
 package com.gmail.nghia241717378.contactapp;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
-    List<Model> list;
-    Context context;
+    private List<Model> list;
+    //private Context context;
 
-    public Adapter(List<Model> list, Context context) {
+    Adapter(List<Model> list, Context context) {
         this.list = list;
-        this.context = context;
+        //this.context = context;
     }
 
     @NonNull
@@ -40,11 +43,35 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), EditActivity.class);
-                intent.putExtra("Name",model.getTxtName());
-                intent.putExtra("DOB",model.getTxtDOB());
-                intent.putExtra("Phone",model.getTxtPhone());
+                intent.putExtra("Name", model.getTxtName());
+                intent.putExtra("DOB", model.getTxtDOB());
+                intent.putExtra("Phone", model.getTxtPhone());
                 v.getContext().startActivity(intent);
-                Toast.makeText(context, model.getTxtPhone(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.imgViewPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                }else {
+                    v.getContext().startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + model.getTxtPhone())));
+                }
+            }
+        });
+
+        holder.imgViewMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", model.getTxtPhone(), null));
+                v.getContext().startActivity(intent);
             }
         });
 
@@ -55,16 +82,19 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         return list.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    static class ViewHolder extends RecyclerView.ViewHolder{
         TextView txtODB, txtName, txtPhone;
-        ImageView imgViewEdit;
+        ImageView imgViewEdit, imgViewPhone, imgViewMessage;
 
-        public ViewHolder(@NonNull View itemView) {
+        ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtName = itemView.findViewById(R.id.txtName);
             txtODB = itemView.findViewById(R.id.txtDOB);
             txtPhone = itemView.findViewById(R.id.txtPhone);
             imgViewEdit = itemView.findViewById(R.id.imgViewEdit);
+            imgViewPhone = itemView.findViewById(R.id.imgViewPhone);
+            imgViewMessage = itemView.findViewById(R.id.imgViewMessage);
+
         }
     }
 }
